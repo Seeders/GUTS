a81b85589e718a9c193c4ec1f1f59c653c29a480
Jared Seeders <jared.seeders@gmail.com>
2025-04-16 20:18:00 -0700

    fixed ballistic projectile fr fr

--- projects/default_project/Scripts/components/js/BallisticProjectile.js
+++ projects/default_project/Scripts/components/js/BallisticProjectile.js
@@ -13,7 +13,6 @@ init({ spawnType, owner, target, stats }) {
     this.stats = stats;
     this.piercedEnemies = [];
     this.ownerStats = this.owner.getComponent("stats").stats;
-    this.modelRenderer = this.owner.getComponent('modelRenderer');
     this.distanceTraveled = 0;
     this.distanceToSpawnParticle = 24;
     
@@ -34,11 +33,23 @@ init({ spawnType, owner, target, stats }) {
     
     // Animation state variables
     this.lastZPosition = this.positionZ;
-    this.modelRenderer.setAnimation('ascend');
+
+    
     this.peakThreshold = this.maxHeight * 0.1; // 10% threshold for idle at peak
     this.currentAnimState = 'ascend';
     if(this.stats.attackSound){
         this.game.audioManager.playSound('attackSounds', this.stats.attackSound);
+    }
+}
+
+setAnimation(anim) {
+
+    if(this.game.config.configs.game.is3D) {
+        this.modelRenderer = this.parent.getComponent('modelRenderer');
+        this.modelRenderer?.setAnimation(anim);
+    } else {
+        this.animator = this.parent.getComponent('animator');
+        this.animator?.setAnimation(anim);
     }
 }
 
@@ -76,7 +87,7 @@ update() {
     // Check if animation state needs to change based on z movement and proximity to peak
     if( xyprogressToTarget < .075 ) {
         if (this.currentAnimState !== 'launch') {
-            this.modelRenderer.setAnimation('launch');
+            this.setAnimation('launch');
             this.currentAnimState = 'launch';
         }
     // } else if( xyprogressToTarget > .95 ) {
@@ -87,19 +98,19 @@ update() {
     } else if (distanceFromPeak <= this.peakThreshold) {
         // We're near the peak of the trajectory
         if (this.currentAnimState !== 'idle') {
-            this.modelRenderer.setAnimation('idle');
+            this.setAnimation('idle');
             this.currentAnimState = 'idle';
         }
     } else if (this.positionZ < this.lastZPosition) {
         // We're descending and not near the peak
         if (this.currentAnimState !== 'descend') {
-            this.modelRenderer.setAnimation('descend');
+            this.setAnimation('descend');
             this.currentAnimState = 'descend';
         }
     } else if (this.positionZ > this.lastZPosition) {
         // We're ascending and not near the peak
         if (this.currentAnimState !== 'ascend') {
-            this.modelRenderer.setAnimation('ascend');
+            this.setAnimation('ascend');
             this.currentAnimState = 'ascend';
         }
     }
