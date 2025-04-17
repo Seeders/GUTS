2f79d5d4a96f31f8c9d60e03e7211b703b321d3b
Jared Seeders <jared.seeders@gmail.com>
2025-04-16 19:53:13 -0700

    fix project loading from localStorage on github

--- engine/Engine.js
+++ engine/Engine.js
@@ -149,7 +149,7 @@ class Engine {
     loadCollections() {
         let currentProject = localStorage.getItem("currentProject");
         let gameData = DEFAULT_PROJECT_CONFIG;
-        if (currentProject && currentProject !== "default_project") {
+        if (currentProject && !(currentProject == "default_project" && window.location.hostname === "localhost")) {
             gameData = JSON.parse(localStorage.getItem(currentProject));   
         }
 
