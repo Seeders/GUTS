8a464e0135eae8f60492a48e0794e77e893c8661
Jared Seeders <jared.seeders@gmail.com>
2025-06-03 13:08:10 -0700

    added config console logs

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,5 +1,9 @@
 const { join } = require("path");
 
+console.log("Puppeteer config loaded");
+console.log("Puppeteer cache directory:", join(__dirname, ".cache", "puppeteer"));
+
+
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
