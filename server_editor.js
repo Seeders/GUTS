97d08f8b21d2399a8febe5f2dfc22f4cd5200176
Jared Seeders <jared.seeders@gmail.com>
2025-06-04 11:49:56 -0700

    use port 80

--- server_editor.js
+++ server_editor.js
@@ -311,7 +311,7 @@ app.post('/api/cache', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Files will be served from: ${PROJS_DIR}`);
