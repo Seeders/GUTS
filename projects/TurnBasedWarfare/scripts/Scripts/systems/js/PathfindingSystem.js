fb4b0f5557af3b5248469d160e79b0e9e3426ace
Jared Seeders <jared.seeders@gmail.com>
2025-11-22 15:09:33 -0800

    nothing is showing impassable

--- projects/TurnBasedWarfare/scripts/Scripts/systems/js/PathfindingSystem.js
+++ projects/TurnBasedWarfare/scripts/Scripts/systems/js/PathfindingSystem.js
@@ -220,11 +220,7 @@ class PathfindingSystem extends GUTS.BaseSystem {
                 // Get unit type definition to check if object blocks movement
                 const unitType = collections.worldObjects?.[worldObj.type];
 
-                // Skip if object doesn't have collision
-                if (!unitType || unitType.impassable === true) {
-                    continue;
-                }
-
+        
                 // Convert terrain tile position to world position using GridSystem
                 // worldObj.x and worldObj.y are in terrain tile coordinates
                 const worldPos = this.game.gameManager.call('tileToWorld', worldObj.x, worldObj.y);
