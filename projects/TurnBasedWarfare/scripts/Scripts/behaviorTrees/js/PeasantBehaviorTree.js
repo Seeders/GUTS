0dd8d971ec4ea8504abceb2f6397457924911fe4
Jared Seeders <jared.seeders@gmail.com>
2025-11-23 16:36:49 -0800

    fixed interfaces refs

--- projects/TurnBasedWarfare/scripts/Scripts/behaviorTrees/js/PeasantBehaviorTree.js
+++ projects/TurnBasedWarfare/scripts/Scripts/behaviorTrees/js/PeasantBehaviorTree.js
@@ -55,14 +55,8 @@ class PeasantBehaviorTree extends GUTS.BaseBehaviorTree {
 
         // Query all entities with RESOURCE component (gold mines)
         // Use gameManager if available, otherwise query directly
-        let mines;
-        if (game.gameManager && game.gameManager.getEntitiesByComponent) {
-            mines = game.gameManager.getEntitiesByComponent('RESOURCE');
-        } else if (game.getEntitiesByComponent) {
-            mines = game.getEntitiesByComponent('RESOURCE');
-        } else {
-            return null;
-        }
+        let mines = game.getEntitiesWith('RESOURCE');
+  
 
         let nearest = null;
         let minDist = Infinity;
